519598796058458bebfdc6576856c857ea81157f
fruit-bird <github.ru3w9@slmail.me>
2024-09-02 22:34:58 +0100

    test takes file path as cli arg

--- tests/upload_pdf.py
+++ tests/upload_pdf.py
@@ -1,11 +1,24 @@
 import json
 import requests
+import argparse
 
 
-pdf_path = "eg01_caring_for_your_cat.pdf"
-url = "http://localhost:8000/pdf/upload"
+def upload_pdf(pdf_path):
+    url = "http://localhost:8000/pdf"
 
-with open(pdf_path, "rb") as file:
-    files = {"file": (pdf_path, file, "application/pdf")}
-    response = requests.post(url, files=files)
-    print(json.dumps(response.json()))
+    with open(pdf_path, "rb") as file:
+        files = {"file": (pdf_path, file, "application/pdf")}
+        response = requests.post(url, files=files)
+        print(json.dumps(response.json(), indent=4))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload a PDF file to a server.")
+    parser.add_argument(
+        "pdf_path",
+        type=str,
+        help="The path to the PDF file you want to upload",
+    )
+
+    args = parser.parse_args()
+    upload_pdf(args.pdf_path)
