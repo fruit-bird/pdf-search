3d4080ebdf18b9a8f307015ff42aa79372f703e4
fruit-bird <github.ru3w9@slmail.me>
2024-09-03 23:56:58 +0100

    type annotated the metadata endpoints

--- pdf_search/routes/metadata_routes.py
+++ pdf_search/routes/metadata_routes.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter
-from pydantic.types import UUID4
+from pydantic.types import UUID4, List
 
 from pdf_search.services import EmbeddingService
+from pdf_search.schemas import UploadPDFResponse
 
 
 router = APIRouter(prefix="/metadata", tags=["metadata"])
 
 
 @router.get("/")
-async def get_metadata() -> list[dict]:
+async def get_metadata() -> List[UploadPDFResponse]:
     return await EmbeddingService.get_metadata()
 
 
 @router.get("/{pdf_source_uuid}")
-async def get_metadata_about_pdf(pdf_source_uuid: UUID4) -> list[dict]:
+async def get_metadata_about_pdf(pdf_source_uuid: UUID4) -> List[UploadPDFResponse]:
     return await EmbeddingService.get_metadata(pdf_uuid=pdf_source_uuid)
